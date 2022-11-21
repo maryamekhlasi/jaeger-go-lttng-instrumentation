d950e71eccb1958cfc5c45ea128063a17154cc25
maryamekhlasi <38199297+maryamekhlasi@users.noreply.github.com>
2022-11-21 10:54:10 -0500

    Create jaeger-tp.h

---
+++ jaeger-tp.h
@@ -0,0 +1,38 @@
+#undef TRACEPOINT_PROVIDER
+#define TRACEPOINT_PROVIDER jaeger_ust
+
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "./jaeger-tp.h"
+
+#if !defined(_TP_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define _TP_H
+
+#include <lttng/tracepoint.h>
+
+TRACEPOINT_EVENT(
+    lttng,
+    event,
+
+    /* Input arguments */
+    TP_ARGS(
+    	char*, o_name,
+    	uint64_t, p_id,
+        uint64_t, t_id,
+        uint64_t, s_id,
+        char*, f_name
+    ),
+
+    /* Output event fields */
+    TP_FIELDS(
+        ctf_string(op_name, o_name)
+        ctf_integer(uint64_t, parent_id, p_id)
+        ctf_integer(uint64_t, trace_id, t_id)
+        ctf_integer(uint64_t, span_id, s_id)
+        ctf_string(fun_name, f_name)
+    )
+)
+
+
+#endif /* _TP_H */
+
+#include <lttng/tracepoint-event.h>
